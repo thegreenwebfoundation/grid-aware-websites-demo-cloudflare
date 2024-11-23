6e93c2b47200a2ef6604b409ff06e3bd40f7840b
fershad <27988517+fershad@users.noreply.github.com>
2024-11-23 18:37:35 +0800

    fix package import

--- workers/co2e/src/index.js
+++ workers/co2e/src/index.js
@@ -8,8 +8,8 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import { gridAwareCO2e, gridAwarePower} from 'grid-aware-websites';
-import {cloudflare} from 'grid-aware-websites/plugins/edge';
+import { gridAwareCO2e, gridAwarePower} from '@greenweb/grid-aware-websites';
+import {cloudflare} from '@greenweb/grid-aware-websites/plugins/edge';
 import {gridAwareRewriter, regularRewriter} from '../../utils';
 
 export default {
